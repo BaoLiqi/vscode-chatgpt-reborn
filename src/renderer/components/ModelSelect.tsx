a24f3b6ef5bc9747a37523f924c62f5e0065cefd
Blq <243987385@qq.com>
2025-04-17 08:52:07 +0800

    update

--- src/renderer/components/ModelSelect.tsx
+++ src/renderer/components/ModelSelect.tsx
@@ -7,7 +7,7 @@ import Icon from "./Icon";
 const models = [
   // { name: "e-g2p", model: Model.gemini_2_pro_exp },
   { name: "e-g25p", model: Model.gemini_25_pro_exp },
-
+  // 1.25 10
   { name: "g25paid", model: Model.gemini_25_pro_preview },
 
   // { name: "e-g2f", model: Model.gemini_2_flash_exp },
@@ -15,8 +15,9 @@ const models = [
   // { name: "g2f-001", model: Model.gemini_2_flash_001 },
   // { name: "dsR1", model: Model.deepseek_r1 },
   { name: "41nano", model: Model.gpt_41_nano },
-  { name: "41mini", model: Model.gpt_41_mini },
+  // { name: "41mini", model: Model.gpt_41_mini },
   { name: "41", model: Model.gpt_41 },
+  { name: "o4-mini", model: Model.gpt_o4_m },
   { name: "dsV3", model: Model.deepseek_v3 },
   { name: "dsV3f", model: Model.deepseek_v3f },
   // { name: "qwq_free", model: Model.qwq_free },
